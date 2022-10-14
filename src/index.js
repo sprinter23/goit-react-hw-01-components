0b3c38d29c2ffc1d5aedbf92aed7ba204cf2b239
Aleksandr Shkola <sprinterr23@gmail.com>
2022-10-14 20:27:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { ThemeProvider } from 'styled-components';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
